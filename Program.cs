9a7bd04e9e364f4070f2bb4ac6190bee1fe654ad
MarinaGorsh <69041379+MarinaGorsh@users.noreply.github.com>
2023-09-09 19:47:25 +0200

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+//using App;
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build();
+
+//app.MapGet("/",async context =>
+//{
+//    var company = new Company("Privat bank", 1992);
+//    await context.Response.WriteAsync($"Company Name: {company.name}, Year of foundation: {company.yearOfCr}");
+//});
+
+//app.Run();
+
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", async context =>
+{
+    var random = new Random();
+    var randomNumber = random.Next(0, 101);
+    await context.Response.WriteAsync($"Random Number: {randomNumber}");
+});
+
+app.Run();
