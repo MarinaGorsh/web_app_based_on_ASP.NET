19efe39a80523751146246f8732344e93eb3e417
MarinaGorsh <69041379+MarinaGorsh@users.noreply.github.com>
2023-09-09 19:46:29 +0200

    Create Company.cs

---
+++ Company.cs
@@ -0,0 +1,14 @@
+namespace App
+{
+    public class Company
+    {
+        public string name;
+        public int yearOfCr;
+
+        public Company(string name, int yearOfCr)
+        {
+            this.name = name;
+            this.yearOfCr = yearOfCr;
+        }
+    }
+}
