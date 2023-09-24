4bd2201cc47ba852827b6c4ed89cb3686572f25c
MarinaGorsh <69041379+MarinaGorsh@users.noreply.github.com>
2023-09-24 12:38:38 +0200

    Update and rename compan.ini to CalcService.cs

---
+++ CalcService.cs
@@ -0,0 +1,25 @@
+namespace WebApplication3
+{
+    public class CalcService
+    {
+        public int Sum(int A, int B)
+        {
+            return A + B;
+        }
+
+        public int Minus(int A, int B)
+        {
+            return A - B;
+        }
+
+        public int Divide(int A, int B)
+        {
+            return A / B;
+        }
+
+        public int Multiple(int A, int B)
+        {
+            return A * B;
+        }
+    }
+}
