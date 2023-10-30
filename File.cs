d50d33377de5c04844e2630713fe2707c5f38ea5
MarinaGorsh <69041379+MarinaGorsh@users.noreply.github.com>
2023-10-30 13:59:47 +0100

    Add files via upload

---
+++ File.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+using System.Text;
+using System.IO;
+
+
+public class FileController : Controller
+{
+    public IActionResult DownloadFile()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult DownloadFileTxt(UserModel model)
+    {
+        string fileName = $"{model.NameOfFile}.txt";
+        string file = $"User Name: {model.UserName}\nUser Surname: {model.UserSurname}";
+        byte[] buffer = Encoding.UTF8.GetBytes(file);
+        return File(buffer, "text/plain", fileName);
+    }
+}
---
+++ UserModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication4.Models
+{
+    public class UserModel
+    {
+        public string UserName { get; set; }
+        public string UserSurname { get; set; }
+        public string NameOfFile { get; set; }
+    }
+}
