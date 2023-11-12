2d5bd7672aafd15aa398e6d325bdf2fe9c4ffd94
MarinaGorsh <69041379+MarinaGorsh@users.noreply.github.com>
2023-11-12 17:13:22 +0100

    Create Weather.cs

---
+++ ex1/Weather.cs
@@ -0,0 +1,9 @@
+namespace WebApplication4.Models
+{
+    public class Weather
+    {
+        public int Temperature { get; set; }
+        public string Description { get; set; }
+        public string Region { get; set; }
+    }
+}
