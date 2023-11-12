1c8877c9a1722c00ffae434cc337ac2f6c390486
MarinaGorsh <69041379+MarinaGorsh@users.noreply.github.com>
2023-11-12 14:21:59 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,22 +1,11 @@
-using WebApplication3;
+var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder();
-builder.Services.AddTransient<CalcService>();
-builder.Services.AddTransient<ITimeService, ShortTimeService>();
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.Map("/ex1", async context =>
-{
-    var calc = app.Services.GetService<CalcService>();
-    await context.Response.WriteAsync($"Sum: {calc.Sum(5, 8)}\n");
-    await context.Response.WriteAsync($"Minus: {calc.Minus(8, 5)}\n");
-    await context.Response.WriteAsync($"Multiple: {calc.Multiple(5, 8)} \n");
-    await context.Response.WriteAsync($"Divide: {calc.Divide(8, 2)} \n");
-});
-app.Map("/ex2", async context =>
-{
-    var time = app.Services.GetService<ITimeService>();
-    await context.Response.WriteAsync($"Time: {time?.GetTime()}");
-});
-app.Run();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}");
 
+app.Run();
